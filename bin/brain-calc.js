3857922bb61d5b12cdd0be9b214be1387df3bb2f
Станислав Плетнев <staspletnev1@gmail.com>
2024-10-08 22:54:03 +0500

    bug fix

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -4,4 +4,4 @@ import { runGame } from '../src/index.js';
 import { generateQuestion, description } from '../src/games/calc.js';
 
 const gameLogic = { generateQuestion, description };
-runGame(gameLogic);
+runGame(gameLogic); 
