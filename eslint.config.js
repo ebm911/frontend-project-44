d0d4f6be26f1bd01d7d4886840a2234e699340ae
Станислав Плетнев <staspletnev1@gmail.com>
2024-10-16 22:14:19 +0500

    fix

--- eslint.config.js
+++ eslint.config.js
@@ -6,15 +6,17 @@ import pluginJs from '@eslint/js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({ baseDirectory: __dirname, recommendedConfig: pluginJs.configs.recommended });
-
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: pluginJs.configs.recommended,
+});
 export default [
   {
     languageOptions: {
       parser: '@babel/eslint-parser',
       parserOptions: {
         ecmaVersion: 2018,
-        sourceType: "module",
+        sourceType: 'module',
         requireConfigFile: false,
         babelOptions: {
           presets: ['@babel/preset-env'],
